d97337a9685c1dd4319bf353c24b6876d8e9dc50
fritterj-fhsu <143357292+fritterj-fhsu@users.noreply.github.com>
2024-01-29 19:51:56 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Personal Info</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 50px;
-        }
-    </style>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 
